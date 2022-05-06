7ede944a14868249159e29d0bbee2b693ef19740
Evan Lauer <lauere@carleton.edu>
2022-05-06 15:16:25 -0500

    added readString() method

--- tokenizer.c
+++ tokenizer.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
 // Read all of the input from stdin, and return a linked list consisting of the
 // tokens.
@@ -15,10 +16,14 @@ Value *tokenize() {
 
     while (charRead != EOF) {
         //create new constype node
-        Value* newNode = cons(charRead, list); //confused what the newcdr is meant to be
+        Value* newConsNode = cons(charRead, list); // the cdr of this should be the head of the current list
+                                                   // (we will create a reverse list first, then reverse it)
+
+        Value* newCarNode; // this will become the node which contains the actual token
         switch (charRead) {
             case ' ' :
                 // case for when the char is a space
+
                 break;
             case '(' :
                 // case for open paren
@@ -30,6 +35,7 @@ Value *tokenize() {
                 // case for open quote (maybe call a helper function?
                 // and the helper function will read until the end of the string
                 // then return, and then we move on to the next token)
+                
                 break;
             case ';' :
                 // case for a comment (when we find this we need to skip the rest of the line)
@@ -46,10 +52,40 @@ Value *tokenize() {
     return revList;
 }
 
+// This function should be called when an open quote is found. 
+// It will read characters from stdin until another quote is found.
+// If a \n or EOF is found before another quote, an error is thrown.
+//
+// BE CAREFUL WITH THIS! ONLY CALL WHEN " IS FOUND
+//
+// This method will consume stdin characters up to *and including* the closing quote
+char *readString()
+{
+    char charRead = (char)fgetc(stdin); // this should be the first character of the string
+
+    char* newString = "\"";
+    
+    while (charRead != EOF && charRead != '\n')
+    {
+        if (charRead == '"')
+        {
+            return strcat(newString, (char)('"'));
+        } else
+        {
+            // add charRead to newString
+            newString = strcat(newString, charRead);
+        }
+        charRead = (char)fgetc(stdin);
+    } // if we reach the end of the this while loop before we return, it means we have an error.
+    printf("Syntax error: Closing quotation (\") expected.\n");
+    texit(1);
+}
+
 // Displays the contents of the linked list as tokens, with type information
 void displayTokens(Value *list){
     //token = car(list);
     printf("%c : %s \n", car(list), car(list)->type);
     if (cdr(list)->type != NULL_TYPE){
         displayTokens(cdr(list));
-    }}
+    }
+}
